cc5dddcb5f2df7ca9a2b21ca08578ead643ee4bd
Elena Keda <elena_keda_92@mail.ru>
2021-03-24 18:20:37 +0300

    feat: fix solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,24 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
+	let res = [];
     if (Array.isArray(arr)) {
         for (let i = 0; i < arr.length; i++) {
-            if (arr[i] == "--discard-next") {
-                arr[i+1] = '';
-                arr[i] = ''
-            }
-            if (arr[i] == "--discard-prev") {
-                arr[i-1] = '';
-                arr[i] = ''
-            }
-            if (arr[i] == "--double-next") {
-                arr[i] = arr[i+1]
-            }
-            if (arr[i] == "--double-prev") {
-                arr[i] = arr[i-1]
-            }
-        } 
-        return arr.filter((item) => typeof item === "number");
+            if (arr[i] != '--discard-next' && arr[i] != '--discard-prev' && arr[i] != '--double-next' && arr[i] != '--double-prev') {
+                res.push(arr[i]);
+            } else if (arr[i] === '--discard-next') {
+                i++;
+            } else if (arr[i] === '--discard-prev' && i != 0 && arr[i - 2] != '--discard-next') {
+                res.pop();
+            } else if (arr[i] === '--double-next'&& i != arr.length - 1) {
+                res.push(arr[i + 1]);
+            } else if (arr[i] === '--double-prev' && i != 0 && arr[i - 2] != '--discard-next') {
+                res.push(arr[i - 1]);
+            } else if (arr[i] === '--double-prev' && arr[i - 2] == '--discard-next') {
+                continue;
+            }		
+        }
+        return res
     } else {
         throw new Error("Error")
     }
